30ec957af673e73024648a408aabdabf95f29e5f
Lesaffrea <alainlesaffre@hotmail.com>
2015-10-25 08:13:08 +0800

    About the file path and graphic

--- code/common.R
+++ code/common.R
@@ -2,24 +2,14 @@
 #
 #   Common file for the Health Hack application
 #
-#
-
-
-if(Curtin == FALSE){
-        orgdata           <-c("/Users/Alain/Documents/My Data Analysis/Health-Hack/data/health_hack_2015_data.txt")
-        preprocessdataset <-c("/Users/Alain/Documents/My Data Analysis/Health-Hack/data/Dataset.Rda")
-        alphadata         <-c("/Users/Alain//Documents/My Data Analysis/Health-Hack/data/alphacode.csv")
-}else{
-        orgdata           <-c("/Users/Alain/Dropbox/HealthHack/Health-Hack/data/Health_hack_2015_data.txt")
-        alphadata         <-c("/Users/Alain/Dropbox/HealthHack/Health-Hack/data/alphacode.csv")
-        preprocessdataset <-c("/Users/Alain/Dropbox/HealthHack/Health-Hack/data/Dataset.Rda")	
-}	
+#   25/10/2015 - Last change for the file path
 
 
 
-# Data set initial read
-typeread <-c(  # "Patient_ID" = "factor",
-               # "Request_ID" = "factor",
-                "date_coln"   ="character",
-                "itemname" = "factor",
-                "ward" = "factor")
+orgdata           <-c("../data/health_hack_2015_data.txt")
+preprocessdataset <-c("../data/Dataset.Rda")
+alphadata         <-c("../data/alphacode.csv")
+
+
+
+
